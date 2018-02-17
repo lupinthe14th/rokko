432988ca2c9106eefded26c6d99d3421d9b7fc78
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-02-17 13:40:50 +0900

    modify MAP Application

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import leaflet from 'leaflet'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidEnter(){
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+      attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
+      mazZoom:18
+    }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom:10
+    }).on('locationfound',(e) => {
+      let markerGroup = leaflet.featureGroup();
+      let marker: any = leaflet.marker([e.latitude,e.longitude]).on('click', () => {
+        alert('Maker clicked');
+      })
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+    }).on('locationerror',(err) => {
+      alert(err.message);
+    })
+  }
 }
